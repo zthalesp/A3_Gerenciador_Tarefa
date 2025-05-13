1e7f41a1c341b2ba9d7f5a95c6cc535b10b27e14
Thales Gabriel de Souza Oliveira <thalesg616@gmail.com>
2025-05-13 11:53:57 -0300

    Create MainFrame.java

---
+++ src/view/MainFrame.java
@@ -0,0 +1,71 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import controller.TarefaController;
+import model.tarefa;
+import view.componetes.TarefaListRenderer;
+
+public class MainFrame extends JFrame {
+    private JList<tarefa> tarefasList;
+    private DefaultListModel<tarefa> listModel;
+    private TarefaController controller;
+
+    public MainFrame() {
+        super("Gerenciador de Tarefas");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        
+        controller = new TarefaController(this);
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        
+        JButton addButton = new JButton("Adicionar");
+        JButton editButton = new JButton("Editar");
+        JButton removeButton = new JButton("Remover");
+        JButton statusButton = new JButton("Alterar Status");
+        
+        addButton.addActionListener(e -> controller.adicionarTarefa());
+        editButton.addActionListener(e -> controller.editarTarefa());
+        removeButton.addActionListener(e -> controller.removerTarefa());
+        statusButton.addActionListener(e -> controller.alterarStatus());
+        
+        buttonPanel.add(addButton);
+        buttonPanel.add(editButton);
+        buttonPanel.add(removeButton);
+        buttonPanel.add(statusButton);
+        
+        add(buttonPanel, BorderLayout.NORTH);
+        
+        
+        listModel = new DefaultListModel<>();
+        tarefasList = new JList<>(listModel);
+        tarefasList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tarefasList.setCellRenderer(new TarefaListRenderer());
+        
+        add(new JScrollPane(tarefasList), BorderLayout.CENTER);
+        
+        
+        controller.carregarTarefas();
+    }
+
+    public DefaultListModel<tarefa> getListModel() {
+        return listModel;
+    }
+
+    public JList<tarefa> getTarefasList() {
+        return tarefasList;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
